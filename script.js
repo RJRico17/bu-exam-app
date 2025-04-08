1e105fe171bfdf2ec34953ba0f795eb382c98449
RJ <fredrickalexander1727@gmail.com>
2025-04-07 23:01:38 -0700

    bug on scoring first drill, scoring issue, work on keeping scre for f1-f8

--- script.js
+++ script.js
@@ -1,24 +1,58 @@
-const f1Score = 4, f2Score = 4, f3Score = 4, f4Score = 4, f5Score = 4, f6Score = 0, f7Score = 0, f8Score = 0;
-const overallScore = 0;
+const userScore = {
+    f1Score: 4,
+    f2Score: 0,
+    f3Score: 0,
+    f4Score: 0,
+    f5Score: 0,
+    f6Score: 0,
+    f7Score: 0,
+    f8Score: 0,
+    overallScore: 0
+}
+const userProgress = {
+    cutProgress: 4,
+    stopProgress: 4,
+    followProgress: 4,
+    drawProgress: 4,
+    stunProgress: 4,
+}
 const resultCell = document.getElementsByClassName("result-cell");
 for (i=0;i<resultCell.length;i++) {
     resultCell[i].onclick = cyclePot;
 }
 function cyclePot() {
-    updateScore();
     if (this.classList.contains("good-pot")) {
+        userScore.f1Score--;
+        if (userProgress.cutProgress===7){
+            userProgress.cutProgress--;
+        }
+        userProgress.cutProgress-=2;
         this.classList.remove("good-pot");
         this.classList.add("bad-pot");
+        document.getElementById("f1-score").innerHTML = userScore.f1Score;
+        this.nextElementSibling.innerHTML = userProgress.cutProgress;
     }
     else if (this.classList.contains("bad-pot")) {
         this.classList.remove("bad-pot");
+        userProgress.cutProgress++;
+        this.nextElementSibling.innerHTML = "";
     }
     else {
+        if (userProgress.cutProgress===7){
+            userProgress.cutProgress=7;
+        }
+        else {
+            userProgress.cutProgress++;
+        }
+        userScore.f1Score++;
         this.classList.add("good-pot");
+        document.getElementById("f1-score").innerHTML = userScore.f1Score;
+        this.nextElementSibling.innerHTML = userProgress.cutProgress;
     }
+    updateScore();
 }
 function updateScore() {
-    output.innerHTML = countScore(f1Score,f2Score,f3Score,f4Score,f5Score,f6Score,f7Score,f8Score);
+    output.innerHTML = countScore(userScore.f1Score,userScore.f2Score,userScore.f3Score,userScore.f4Score,userScore.f5Score,userScore.f6Score,userScore.f7Score,userScore.f8Score);
 }
 function countScore(a,b,c,d,e,f,g,h) {
     return a+b+c+d+e+f+g+h;
